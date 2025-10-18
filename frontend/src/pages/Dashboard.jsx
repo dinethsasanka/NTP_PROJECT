cc865546f8d04ced6cc2ddde531a85f8e4adf6c7
CptSparrow <dinethsasanka2002@gmail.com>
2025-10-19 04:23:28 +0530

    Add two dashboard.jsx, pages and components

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import DashboardCore from "../components/Dashboard";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <Navbar />
+      <DashboardCore />
+    </div>
+  );
+}
